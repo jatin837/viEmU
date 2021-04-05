45e1aacedc78103090461343efc2d3742db19e0e
j0hnbl4k3 <pandeyjatin47@gmail.com>
2021-04-06 01:04:03 +0530

    turn off more flags

--- main.c
+++ main.c
@@ -14,10 +14,12 @@ void disable_raw_mode(void);
 void enable_raw_mode() {
 	tcgetattr(STDIN_FILENO, &orig_termios);
 	atexit(disable_raw_mode);
+
 	struct termios raw = orig_termios;
 	raw.c_lflag = raw.c_lflag & ~(ECHO | ICANON | ISIG | IEXTEN);
-	raw.c_iflag = raw.c_iflag & ~(IXON | ICRNL);
+	raw.c_iflag = raw.c_iflag & ~(BRKINT | ICRNL | INPCK |IXON | ISTRIP);
 	raw.c_oflag = raw.c_oflag & ~(OPOST);
+	raw.c_cflag |= (CS8);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -32,7 +34,7 @@ int main(int argc, char **argv){
 	char c; 
 	while (read(STDIN_FILENO, &c, 1) >= 1 && c != 'q') {
 		if (iscntrl(c)) {
-			printf("%c\r\n", c);
+			printf("%d\r\n", c);
 		} else {
 			printf("%d ('%c')\r\n", c, c);
 		}
