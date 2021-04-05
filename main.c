87a16e086a4b39b73a40f43b70f6a0108d4df997
j0hnbl4k3 <pandeyjatin47@gmail.com>
2021-04-05 16:38:34 +0530

    turn of canonical mode
    TODO
    
    display keypresses

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ void enable_raw_mode() {
 	tcgetattr(STDIN_FILENO, &orig_termios);
 	atexit(disable_raw_mode);
 	struct termios raw = orig_termios;
-	raw.c_lflag = raw.c_lflag & ~(ECHO);
+	raw.c_lflag = raw.c_lflag & ~(ECHO | ICANON);
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
