1701249811b72dc5bad46ddfb4e5b5e5272c9e67
j0hnbl4k3 <pandeyjatin47@gmail.com>
2021-04-05 17:03:26 +0530

    disabled alog of cntrl keys

--- main.c
+++ main.c
@@ -15,7 +15,9 @@ void enable_raw_mode() {
 	tcgetattr(STDIN_FILENO, &orig_termios);
 	atexit(disable_raw_mode);
 	struct termios raw = orig_termios;
-	raw.c_lflag = raw.c_lflag & ~(ECHO | ICANON);
+	raw.c_lflag = raw.c_lflag & ~(ECHO | ICANON | ISIG | IEXTEN);
+	raw.c_iflag = raw.c_iflag & ~(IXON | ICRNL);
+
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
