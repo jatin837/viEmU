c6511cb1cd7b3008be06114cd21a05f103fbcbcd
j0hnbl4k3 <pandeyjatin47@gmail.com>
2021-04-09 16:32:33 +0530

    create function to handle keypress and stop showing keypresses

--- main.c
+++ main.c
@@ -48,19 +48,31 @@ void disable_raw_mode() {
 		die("tcsetatttr failed");
 }
 
+char editor_read_key() {
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1){
+		if (nread == -1 && errno != EAGAIN)
+			die("read");
+	}
+	return c;
+
+}
+
+void editor_process_keypress() {
+	char c = editor_read_key();
+
+	switch (c) {
+		case CTRL_KEY('q'):
+			exit(0);
+			break;
+	}
+}
+
 int main(int argc, char **argv){
 	enable_raw_mode();
 	
-	while (1) {
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-			die("READ");
-		if (iscntrl(c)) 
-			printf("%d\r\n", c);
-		else 
-			printf("%d => %c \r\n", c, c);
-		if (c == CTRL_KEY('q')) 
-			break;
-	}
+	while (1)
+		editor_process_keypress();
 	return 0;
 }
